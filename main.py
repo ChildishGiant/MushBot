12645f80681a3cc70f33d810c08ced26bb91107b
SolarPolarMan <solarpolarman@gmail.com>
2016-03-23 22:55:55 +0000

    jiggled things about

--- main.py
+++ main.py
@@ -26,15 +26,10 @@ async def on_message(msg):
 
 	if not msg.channel.is_private:
 
-		for meme in settings.textCommands:
-			if meme in msg.content.lower():
-				await client.send_message(msg.channel, settings.textCommands[meme])
-
-
-
 		#logging messages
 		ts = msg.timestamp
 		targetfile = "logs/{}{}{}/{}.txt".format(ts.year,ts.month,ts.day,msg.channel.name)
+
 		#message format is [hh:mm] auhor - (id): message
 		formattedline = "[{}:{}] {} - ({}): ".format(ts.hour, ts.minute, msg.author.name, msg.author.id)
 		if len(msg.attachments) > 0:
@@ -44,54 +39,67 @@ async def on_message(msg):
 			formattedline += embedslinks
 		else:
 			formattedline += msg.content
+
 		#create a folder for the day if there isn't already one
 		if not os.path.isdir("{}{}{}".format(ts.year,ts.month,ts.day)):
 			os.mkdir("{}{}{}".format(ts.year,ts.month,ts.day))
+
 		#use UTF-8 encoding to handle non-ascii names
 		with open(targetfile, "ab") as f:
 			f.write((formattedline+"\n").encode('utf-8'))
-		
-		#command handleing + making sure they're not on cooldown
-		if msg.startswith('!') and not naughtyList[msg.author]:
 
-			#command cooldown for those not worthy enough.
-			if msg.author not in (serverinfo.modList(client) or serverinfo.codererList()):
-				global naughtyList
-				naughtyList = {}
-				tokens.giveToken(naughtyList,msg.author,"")
-				threading.Timer(settings.commandCooldownTime,tokens.takeToken(naughtyList,msg.author))
+		#Checks if the user is on cooldown
+		if not naughtyList[msg.author]:
 
-			args = msg.split()
-			#command is the first word in the message
-			command = args[0]
-			
-			#check for a corresponding .py file
-			if os.path.isfile(command.replace('!','')+'.py'):
-			
-				#Get the appropriate module name for the command. Caps matter, so all lower case
-				module = command.replace('!','').lower()
-				
-				#dynamically import the module required, then refresh it to make sure we're running the newest possible code
-				command = __import__(module)
-				reload(command)
-				
-				#actually run the command, command.py file must have a run() method that takes these 4 arguments
-				await command.run(client, msg.channel, msg.author, args[1:])
-			else:
-				#no command.py file, instead check for a matching image file in the images folder and upload it.
-				if '.' in args[0] or '/' in args[0]: #no sketchy folder tricks.
-					return
-					
-				imagefile = 'images/'+args[0].replace('!','')
-				
-				if os.path.isfile(imagefile+'.jpg'):
-					await client.send_file(c, imagefile+'.jpg')
-			
-				if os.path.isfile(imagefile+'.png'):
-					await client.send_file(c, imagefile+'.png')
-					
-				if os.path.isfile(imagefile+'.gif'):
-					await client.send_file(c, imagefile+'.gif')
+			#Check for all text commands
+			for meme in settings.textCommands:
+				if meme in msg.content.lower():
+					await client.send_message(msg.channel, settings.textCommands[meme])
+
+			#command handleing
+			if msg.startswith(settings.activator) :
+
+				#command cooldown for those not worthy enough.
+				if msg.author not in (serverinfo.modList(client) or serverinfo.codererList()):
+					global naughtyList
+					naughtyList = {}
+					tokens.giveToken(naughtyList,msg.author,"")
+					threading.Timer(settings.commandCooldownTime,tokens.takeToken(naughtyList,msg.author))
+
+				#command is the first word in the message
+				args = msg.split()
+				command = args[0]
+
+				#check for a corresponding .py file
+				if os.path.isfile(command.replace('!','')+'.py'):
+
+					#Get the appropriate module name for the command. Caps matter, so all lower case
+					module = command.replace('!','').lower()
+
+					#dynamically import the module required, then refresh it to make sure we're running the newest possible code
+					command = __import__(module)
+					reload(command)
+
+					#actually run the command, command.py file must have a run() method that takes these 4 arguments
+					try:
+						await command.run(client, msg.channel, msg.author, args[1:])
+					except NameError:
+						await client.send_message(msg.channel, settings.invalidCommand)
+				else:
+					#no command.py file, instead check for a matching image file in the images folder and upload it.
+					if '.' in args[0] or '/' in args[0]: #no sketchy folder tricks.
+						return
+
+					imagefile = 'images/'+args[0].replace('!','')
+
+					if os.path.isfile(imagefile+'.jpg'):
+						await client.send_file(c, imagefile+'.jpg')
+
+					if os.path.isfile(imagefile+'.png'):
+						await client.send_file(c, imagefile+'.png')
+
+					if os.path.isfile(imagefile+'.gif'):
+						await client.send_file(c, imagefile+'.gif')
 	
 #on_ready event, triggers once the client is logged in and ready.
 @client.event
