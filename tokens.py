76f28354ef5a24a3e866b7379cb16982f5eb85a7
SolarPolarMan <solarpolarman@gmail.com>
2016-03-23 22:24:22 +0000

    Gave birth to the token framework

---
+++ tokens.py
@@ -0,0 +1,15 @@
+import main
+
+
+def giveToken(dictName,user, token):
+    if not dictName:
+        return None
+    else:
+        dictName[user] = token
+
+
+def takeToken(dictName,user):
+    if dictName[user]:
+        del dict[user]
+    else:
+        return None
