3df46c34156e369558776f0cc32018fb0e5b40b8
SolarPolarMan <solarpolarman@gmail.com>
2016-03-23 19:45:16 +0000

    Fixed previous commit.

---
+++ emote.py
@@ -0,0 +1,16 @@
+from urllib import request as requests
+import os
+
+#get a list of all global emotes
+emotes = requests.get("http://twitchemotes.com/api_cache/v2/global.json").json()
+
+def emote(msg):
+    em = msg.content
+    r = requests.get("http://static-cdn.jtvnw.net/emoticons/v1/" + str(emotes["emotes"][em]["image_id"]) + "/1.0")
+    if r.status_code == 200:
+        if not os.path.isfile("emotes/" + em + ".png"):
+            with open("emotes/" + em + ".png", "wb") as f:
+                f.write(r.content)
+        return msg.channel, "emotes/" + em + ".png"
+    else:
+        print("Emote " + em + " returned status code " + r.status_code)
