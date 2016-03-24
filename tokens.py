e059ff58f92cb6adcbd4d877f74465885412cad4
SolarPolarMan <solarpolarman@gmail.com>
2016-03-24 13:02:44 +0000

    That didn't work.

--- tokens.py
+++ tokens.py
@@ -1,5 +1,4 @@
 def giveToken(dictName,user, token):
-    global dictName
     if not dictName:
         return None
     else:
@@ -7,14 +6,12 @@ def giveToken(dictName,user, token):
 
 
 def takeToken(dictName, user):
-    global dictName
     if dictName[user]:
         del dict[user]
     else:
         return None
 
 def checkToken(dictName, user):
-    global dictName
     try:
         if dictName[user]:
             return True
