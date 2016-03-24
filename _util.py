a95bab152a2f53afb768ea6fb274592be85fe9e5
SolarPolarMan <solarpolarman@gmail.com>
2016-03-24 12:47:06 +0000

    Added makeBlankFile()

--- _util.py
+++ _util.py
@@ -3,3 +3,10 @@ def getServerByName(client, n):
     for server in client.servers:
         if server.name == n:
             return server
+
+def makeBlankFile(filename):
+    #checks to see if file exists
+    if os.path.isfile(filename):
+    else:
+        #make file
+        open(filename, 'a').close()
