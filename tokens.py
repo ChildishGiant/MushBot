5d64cd529316e63c5546b06f918f5d7ad538256d
SolarPolarMan <solarpolarman@gmail.com>
2016-03-24 12:59:31 +0000

    must be using the global dict.

--- tokens.py
+++ tokens.py
@@ -1,4 +1,5 @@
 def giveToken(dictName,user, token):
+    global dictName
     if not dictName:
         return None
     else:
@@ -6,12 +7,14 @@ def giveToken(dictName,user, token):
 
 
 def takeToken(dictName, user):
+    global dictName
     if dictName[user]:
         del dict[user]
     else:
         return None
 
 def checkToken(dictName, user):
+    global dictName
     try:
         if dictName[user]:
             return True
