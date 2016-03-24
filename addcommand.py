924bf116cde4cfc2f75c302d845f5ed7f6fce1a5
SolarPolarMan <solarpolarman@gmail.com>
2016-03-24 12:47:31 +0000

    Editied to use makeBlankFile()

--- addcommand.py
+++ addcommand.py
@@ -1,21 +1,18 @@
 import os
 import settings
+import _util
 
 def run(client, msg, args):
-    #checks to see if ascii.txt exists
-    if os.path.isfile("ascii.txt"):
-        if "|DIVIDER|" not in msg.content:
-            if len(args) == "2":
-                call = args[0]
-                response = args[1]
-                with open("ascii.txt", "") as a
-                    a.write(call+"|DIVIDER|"+response+os.linesep)
-                    return "Added command:`"+settings.activator+call+"`"
-            else:
-                return "Example: `!addcommand lenny ( ͡° ͜ʖ ͡°)`"
+    _util.makeBlankFile("ascii.txt")
+    if "|DIVIDER|" not in msg.content:
+        if len(args) == "2":
+            call = args[0]
+            response = args[1]
+            with open("ascii.txt", "") as a
+                a.write(call+"|DIVIDER|"+response+os.linesep)
+                return "Added command:`"+settings.activator+call+"`"
         else:
-            #if they're trying to break it.
-            return settings.malicous
+            return "Example: `!addcommand lenny ( ͡° ͜ʖ ͡°)`"
     else:
-        #make ascii.txt
-        open("ascii.txt", 'a').close()
+        #if they're trying to break it.
+        return settings.malicous
