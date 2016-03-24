8952f03b4a9bdaadd836185ac8794b2b051f1d38
SolarPolarMan <solarpolarman@gmail.com>
2016-03-24 11:49:08 +0000

    added checkToken

--- tokens.py
+++ tokens.py
@@ -5,8 +5,15 @@ def giveToken(dictName,user, token):
         dictName[user] = token
 
 
-def takeToken(dictName,user):
+def takeToken(dictName, user):
     if dictName[user]:
         del dict[user]
     else:
         return None
+
+def checkToken(dictName, user):
+    try:
+        if dictName[user]:
+            return True
+    except NameError:
+        return False
