b7545816a6b6c1619eb08c0b127ca66d2f023323
SolarPolarMan <solarpolarman@gmail.com>
2016-03-24 20:40:16 +0000

    Added timeForPoint var

--- settings.py
+++ settings.py
@@ -2,3 +2,6 @@ commandCooldownTime = 15.0
 activator = "!"
 invalidCommand = "That's not a real command, you idiot."
 malicous = "Stop trying to break things."
+
+#the time it takes to get one point while online in mins
+timeForPoint = 5
