d36908040c8dd421dae89dc8429c818998788e35
SolarPolarMan <solarpolarman@gmail.com>
2016-03-24 21:51:04 +0000

    Updated to use settings.divider

--- addcommand.py
+++ addcommand.py
@@ -5,12 +5,12 @@ import _util
 async def run(client, msg, args):
     if msg.author in serverinfo.modList or serverinfo.codererList:
         _util.makeBlankFile("ascii.txt")
-        if "|DIVIDER|" not in msg.content:
+        if settings.divider not in msg.content:
             if len(args) > 2:
                 call = args[0]
                 response = " ".join(args[1:])
                 with open("ascii.txt", "ab") as a:
-                    a.write((call+"|DIVIDER|"+response+os.linesep).encode('utf-8'))
+                    a.write((call+settings.divider+response+os.linesep).encode('utf-8'))
                     await client.send_message(msg.channel, "Added command:`"+call+"`")
             else:
                 await client.send_message(msg.channel, "Example: `" + settings.activator + "addcommand lenny ( ͡° ͜ʖ ͡°)`")
