8bea175b823913e3f1bae81a64c69be702e076a1
SolarPolarMan <solarpolarman@gmail.com>
2016-03-24 21:49:46 +0000

    Wrote points.give()

--- points.py
+++ points.py
@@ -1,5 +1,20 @@
 import _util
+import settings
 
-def give(user, amount):
+def give(fileName, user, amount):
+    _util.makeBlankFile(fileName)
+    if settings.divider in user.name:
+
     amount = str(amount)
+    with open(fileName, "r") as f:
+        data = f.readlines()
+
+    for line in data
+        if line.startswith(user.id):
+            uid = line.split(settings.divider)[0]
+            points = line.split(settings.divider)[1] + amount
+            data[data.index(line)] = uid+settings.divider+points
+
+            with open(fileName, "w") as f:
+                f.writelines( data )
     return
