0e4489e6e2fe08d244f1b206684c832e23ecc663
SolarPolarMan <solarpolarman@gmail.com>
2016-03-24 12:31:10 +0000

    Added code to make ascii.txt

--- addcommand.py
+++ addcommand.py
@@ -2,14 +2,20 @@ import os
 import settings
 
 def run(client, msg, args):
-    if "|DIVIDER|" not in msg.content:
-        if len(args) == "2":
-            call = args[0]
-            response = args[1]
-            with open("ascii.txt", "") as a
-                a.write(call+"|DIVIDER|"+response+os.linesep)
-                return "Added command:`"+settings.activator+call+"`"
+    #checks to see if ascii.txt exists
+    if os.path.isfile("ascii.txt"):
+        if "|DIVIDER|" not in msg.content:
+            if len(args) == "2":
+                call = args[0]
+                response = args[1]
+                with open("ascii.txt", "") as a
+                    a.write(call+"|DIVIDER|"+response+os.linesep)
+                    return "Added command:`"+settings.activator+call+"`"
+            else:
+                return "Example: `!addcommand lenny ( ͡° ͜ʖ ͡°)`"
         else:
-            return "Example: `!addcommand lenny ( ͡° ͜ʖ ͡°)`"
+            #if they're trying to break it.
+            return settings.malicous
     else:
-        return settings.malicous
+        #make ascii.txt
+        open("ascii.txt", 'a').close()
