968b6f3dea027030e39bd0afa70621de89b1cb4a
SolarPolarMan <solarpolarman@gmail.com>
2016-03-28 12:11:07 +0100

    added one line?

--- points.py
+++ points.py
@@ -1,5 +1,6 @@
 import _util
 import settings
+import time
 
 def give(fileName, user, amount):
     _util.makeBlankFile("data/"+fileName)
@@ -18,3 +19,13 @@ def give(fileName, user, amount):
             with open("data/"+fileName, "w") as f:
                 f.writelines( data )
     return
+#only to be called in a thread.
+def tip():
+    while True:
+    	if time.time() % (settings.timeForPoint**60) == 0:
+    		#create shekels list (they're called points in the code because it's easier to type and I keep typing SHMEKELS)
+    		_util.makeBlankFile("points.txt")
+    		for member in serverinfo.server(client).members:
+    			#checks if user is active
+    			if member.status == discord.status.online:
+    				give("points.txt", msg.author, 1)
